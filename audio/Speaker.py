402dc15f884adc997565dd8665b4044d75ca3566
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-08-14 02:45:04 +0530

    <Fix: pyttsx3>?

--- audio/Speaker.py
+++ audio/Speaker.py
@@ -1,9 +1,13 @@
 import subprocess
+import pyttsx3
 
 
 class Speaker:
     def __init__(self):
-        pass
+        self.engine = pyttsx3.init()
+        self.engine.setProperty("voice", "english")
 
     def speak(self, text: str):
-        subprocess.call(["espeak", "-ven-us+f5", "-s180", text])
+        # subprocess.call(["espeak", "-ven-us+f5", "-s180", text])
+        self.engine.say(text)
+        self.engine.runAndWait()
